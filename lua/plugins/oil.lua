0e307c3f6e0a4af97f4fb462d50b394e27956503
Alexander <lebo8020@mylaurier.ca>
2024-05-30 11:43:58 -0400

    modularized kickstart initial plugins, added oil.lua, copilot

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      columns = {
+        'icon',
+        'mtime',
+      },
+    },
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
