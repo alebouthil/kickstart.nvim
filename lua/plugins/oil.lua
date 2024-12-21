c7f2d5121c29c7cf538f199b9e356eb5093487e7
Alex <lebo8020@mylaurier.ca>
2024-12-21 14:58:48 -0500

    added parent dir keymap, insp by TJdevries

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,8 +6,12 @@ return {
         'icon',
         'mtime',
       },
+      view_options = {
+        show_hidden = true,
+      },
     },
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    vim.keymap.set('n', '<space>-', require('oil').toggle_float),
   },
 }
