e4f2a97070014d3aafe6dfb8577ae49c369f642b
Alex <lebo8020@mylaurier.ca>
2024-12-21 15:05:46 -0500

    change oil setup, remove floating parent directory

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,14 @@
 return {
   {
     'stevearc/oil.nvim',
-    opts = {
-      columns = {
-        'icon',
-        'mtime',
-      },
-      view_options = {
-        show_hidden = true,
-      },
-    },
-    -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    vim.keymap.set('n', '<space>-', require('oil').toggle_float),
+    config = function()
+      require('oil').setup {
+        columns = { 'icon', 'date' },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+    end,
   },
 }
