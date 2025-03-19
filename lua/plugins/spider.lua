8a9dbebb4053fc5500a21f1a166733da4517eda8
alebouthil <lebo8020@mylaurier.ca>
2025-03-19 16:08:22 -0400

    added spider for line parsing

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,11 @@
+-- Spider is used for better parsing of the W,E,B motions
+return {
+  {
+    'chrisgrieser/nvim-spider',
+    keys = {
+      { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
+    },
+  },
+}
