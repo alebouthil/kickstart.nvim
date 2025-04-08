6151ccad189689dc3c6fbfe6ce45701312678323
alebouthil <lebo8020@mylaurier.ca>
2025-04-08 13:21:24 -0400

    added the trouble.nvim plugin, and updated keymaps to reflect

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>cD',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = '[C]ode [D]iagnostics (Project)',
+      },
+      {
+        '<leader>cd',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = '[C]ode [D]iagnostics (Buffer)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = '[S]ymbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = '[C]ode [L]SP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>ge',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = '[G]oto [E]rrors (Trouble)',
+      },
+      {
+        '<leader>Q',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = '[Q]uickfix List (Trouble)',
+      },
+    },
+  },
+}
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -45,6 +45,7 @@ return {
       -- Document existing key chains
       require('which-key').add {
         { '<leader>c', group = '[C]ode' },
+        { '<leader>g', group = '[G]oto' },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
