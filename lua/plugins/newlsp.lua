474bd0eaffbdb906f06e1a14c6f10725425ff254
Alexander LeBouthillier <lebo8020@mylaurier.ca>
2025-10-16 11:42:03 -0400

    added css support to lsp

--- lua/plugins/newlsp.lua
+++ lua/plugins/newlsp.lua
@@ -56,6 +56,9 @@ return {
       lspconfig.ts_ls.setup {
         capabilities = capabilities,
       }
+      lspconfig.cssls.setup {
+        capabilities = capabilities,
+      }
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
